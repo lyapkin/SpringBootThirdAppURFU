50993ff607d98aa187e7cabc2be7b4af2f7d803c
lyapkin <lyapkin992@gmail.com>
2023-10-07 13:07:44 +0500

    init

---
+++ src/main/java/ru/liapkin/springbootthirdappurfu/SpringBootThirdAppUrfuApplication.java
@@ -0,0 +1,13 @@
+package ru.liapkin.springbootthirdappurfu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThirdAppUrfuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootThirdAppUrfuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/liapkin/springbootthirdappurfu/SpringBootThirdAppUrfuApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.liapkin.springbootthirdappurfu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThirdAppUrfuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
